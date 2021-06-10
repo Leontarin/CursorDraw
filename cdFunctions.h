6f4bce2ae4c37b9095f86e00b12495e5ca375f45
leolight214 <69012235+leolight214@users.noreply.github.com>
2021-06-10 11:23:51 +0300

    Add files via upload

---
+++ cdFunctions.h
@@ -0,0 +1,26 @@
+#ifndef CDFUNCTIONS_H_INCLUDED
+#define CDFUNCTIONS_H_INCLUDED
+#include<conio.h>
+#include<windows.h>
+#include<iostream>
+using namespace std;
+
+int cdMain();
+
+void draw(int i,char input,int hidden,int eraseMode);
+
+void cdDrawOutine(int eraseMode);
+
+void cdHiddenStatus();
+
+void cdColorStatus(int i,int hidden, int eraseMode);
+
+void cdStartupSettings(int i,int hidden,int eraseMode);
+
+void cdEraseDrawing(int i,int hidden,short mouseX,short mouseY,int eraseMode);
+
+void cdCls();
+
+void cls();
+
+#endif
